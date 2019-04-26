cf83d99583ed9d07d3f67ea87d9ec3909a70c9d8
lxclxclxclxc <50006993+lxclxclxclxc@users.noreply.github.com>
2019-04-26 12:41:23 +0800

    Update Data.java

--- Data.java
+++ Data.java
@@ -10,6 +10,16 @@ public class Data {
 	public  int[][] initalState2 = new int[4][4];
 	
 	public  int[][] LT = {{0,14,11,0},
-			              {14,23,27,11}};
+			      {14,23,27,11}};
+	
+	public  int[][] MT = {{0,14},
+			      {14,23}};	
+	
+	public  int[][] ST = {{14,11}};
+	
+	public  int[][] SQ = {{14,11},
+		              {12,13}};
+
+	public  int[][] PA = {{14,23,13}};
 	
 }
